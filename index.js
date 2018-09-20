e034e7f3c46d9b438d60804ffacb6fbb7b32c298
shenwenjie <15805199474@163.com>
2018-09-20 15:01:13 +0800

    重写方法

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 const API = {};
 
+
+/**
+ * 默认toFixed方法为四舍六入五成双算法
+ * 重写toFixed方法调整为四舍五入算法
+ */
+Number.prototype.toFixed = (length == 0) => {
+    length = +length;
+    let carry = 0; //存放进位标志
+    let num, multiple; //num为原浮点数放大multiple倍后的数，multiple为10的length次方
+    let str = this + ''; //将调用该方法的数字转为字符串
+    let dot = str.indexOf("."); //找到小数点的位置
+    if (str.substr(dot + length + 1, 1) >= 5) carry = 1; //找到要进行舍入的数的位置，手动判断是否大于等于5，满足条件进位标志置为1
+    multiple = Math.pow(10, length); //设置浮点数要扩大的倍数
+    num = Math.floor(this * multiple) + carry; //去掉舍入位后的所有数，然后加上我们的手动进位数
+    let result = num / multiple + ''; //将进位后的整数再缩小为原浮点数
+    /*
+     * 处理进位后无小数
+     */
+    dot = result.indexOf(".");
+    if (dot < 0) {
+        result += '.';
+        dot = result.indexOf(".");
+    }
+    /*
+     * 处理多次进位
+     */
+    let len = result.length - (dot + 1);
+    if (len < length) {
+        for (let i = 0; i < length - len; i++) {
+            result += 0;
+        }
+    }
+    return result;
+}
+
 //生成num个0的字符串
 function _makeZero(num) {
     let str = '';
@@ -145,44 +180,12 @@ function execute(x, y, op, acc) {
 
     //小数位数处理
     if (acc) {
-        return Number(Number(result).toFixed(parseInt(acc)));
+        return Number(result).toFixed(acc);
     } else {
         return Number(result);
     }
 }
 
-/**
- * 默认toFixed方法为四舍六入五成双算法
- * 重写toFixed方法调整为四舍五入算法
- */
-Number.prototype.toFixed = function(length) {
-    let carry = 0; //存放进位标志
-    let num, multiple; //num为原浮点数放大multiple倍后的数，multiple为10的length次方
-    let str = this + ''; //将调用该方法的数字转为字符串
-    let dot = str.indexOf("."); //找到小数点的位置
-    if (str.substr(dot + length + 1, 1) >= 5) carry = 1; //找到要进行舍入的数的位置，手动判断是否大于等于5，满足条件进位标志置为1
-    multiple = Math.pow(10, length); //设置浮点数要扩大的倍数
-    num = Math.floor(this * multiple) + carry; //去掉舍入位后的所有数，然后加上我们的手动进位数
-    let result = num / multiple + ''; //将进位后的整数再缩小为原浮点数
-    /*
-     * 处理进位后无小数
-     */
-    dot = result.indexOf(".");
-    if (dot < 0) {
-        result += '.';
-        dot = result.indexOf(".");
-    }
-    /*
-     * 处理多次进位
-     */
-    let len = result.length - (dot + 1);
-    if (len < length) {
-        for (let i = 0; i < length - len; i++) {
-            result += 0;
-        }
-    }
-    return result;
-}
 
 //加法运算
 API.add = function(x, y, acc) {
