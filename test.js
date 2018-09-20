a0f0b0840dd2b40a6a6882c45e8e9e8aba63d99d
shenwenjie <15805199474@163.com>
2018-09-20 12:06:01 +0800

    单元测试

--- test.js
+++ test.js
@@ -2,14 +2,10 @@
 
 var test = require('tape')
 var arithmetic = require('./')
-console.log(arithmetic)
+console.log(arithmetic.add('2', 1), 1111111111)
 test(function (t) {
   t.equal(arithmetic.add('2', 1), 3)
   t.equal(arithmetic.add(4, 1), 5)
   t.equal(arithmetic.subtract(4, 1), 3)
-  t.equal(arithmetic.subtract(4, 1, 3), 3)
-  t.equal(arithmetic.multiply(4, 1, 1), 3)
-  t.equal(arithmetic.divide(4, 1), 3)
-  t.equal(arithmetic.divide(1, 3, 6), 3)
   t.end()
 })
