34bcc430bd11c3f600265726331e499c3965970d
shenwenjie <15805199474@163.com>
2018-09-20 11:26:44 +0800

    Travis CI uodate

---
+++ test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var test = require('tape')
+var arithmetic = require('./')
+
+test(function (t) {
+  t.equal(arithmetic.add('2', 1), 3)
+  t.equal(arithmetic.add(4, 1), 5)
+  t.equal(arithmetic.subtract(4, 1), 3)
+  t.equal(arithmetic.subtract(4, 1, 3), 3)
+  t.equal(arithmetic.multiply(4, 1, 1), 3)
+  t.equal(arithmetic.divide(4, 1), 3)
+  t.equal(arithmetic.divide(1, 3, 6), 3)
+  t.end()
+})
