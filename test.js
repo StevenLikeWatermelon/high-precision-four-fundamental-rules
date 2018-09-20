9eb54f09e79a906bfe2d5519ea53509771cf51f3
shenwenjie <15805199474@163.com>
2018-09-20 12:34:31 +0800

    debug test

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ test(function (t) {
   t.equal(arithmetic.add(4, 1), 5)
   t.equal(arithmetic.subtract(4, 1), 3)
   t.equal(arithmetic.subtract(4, 1, 3), 3)
-  t.equal(arithmetic.multiply(4, 1, 1), 3)
+  t.equal(arithmetic.multiply(4, 1, 1), 4)
   t.equal(arithmetic.divide(4, 1), 4)
   t.equal(arithmetic.divide(1, 3, 6), 0.333333)
   t.equal(arithmetic.divide(1, 3, 3), 0.333)
